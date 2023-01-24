b31ca107bcf762151c7c73b78752d530fd51c3ae
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2023-01-24 01:51:53 +0000

    change the error alert to show the individual errors

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Alert = (props) => {
     return (
-        <div className={`alert ${props.type}`} role="alert">
+        <div className={`alert ${props.type} fade show`} role="alert">
             {props.message}
         </div>
     )
